2d53befbced05d274adec3ae231deef75c9fd558
Tibor Sandor <tiborsandor2001@gmail.com>
2021-09-11 14:06:33 +0200

    add description of control points

--- components/LeftPanel.tsx
+++ components/LeftPanel.tsx
@@ -13,19 +13,22 @@ interface Props {
 
 const LeftPanel: React.FC<Props> = ({ p, reset }) => {
   return (
-    <div className="grid grid-cols-2 m-4 my-auto sm:w-3/12 sm:flex flex-col place-items-center">
-      {p.map((elem, index) => (
-        <DisplayCoords key={index} index={index + 1} coord={elem} />
-      ))}
-      <div className="p-2 col-span-2 mb-10">
-        <button
-          className="bg-gray-800 text-gray-100 p-1 px-2 rounded-md flex justify-center"
-          onClick={reset}
-        >
-          Reset
-        </button>
+    <>
+      <div className="grid grid-cols-2 m-4 my-auto sm:w-3/12 sm:flex flex-col place-items-center">
+        <p className="col-span-2">A kontrol pontok egérrel elhúzhatóak.</p>
+        {p.map((elem, index) => (
+          <DisplayCoords key={index} index={index + 1} coord={elem} />
+        ))}
+        <div className="p-2 col-span-2 mb-10">
+          <button
+            className="bg-gray-800 text-gray-100 p-1 px-2 rounded-md flex justify-center"
+            onClick={reset}
+          >
+            Reset
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
